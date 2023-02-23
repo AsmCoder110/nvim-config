6d5f451a4022340e7252c4bece860d31d999f2bb
Yogendra Singh Kashyap <yskthecpplover@gmail.com>
2023-02-23 21:33:09 +0530

    Trouble.nvim keybindings.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -16,3 +16,23 @@ vim.keymap.set("n", "<leader>l",
     ":lua require('lazy.util').float_term(nil, { cwd = vim.fn.getcwd(), })<CR>")
 
 vim.keymap.set("x", "Y", ":w !wl-copy<CR><CR>")
+
+-- Trouble.nvim
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+               { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+               { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+               { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+               { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+               { silent = true, noremap = true }
+)
+vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+               { silent = true, noremap = true }
+)
