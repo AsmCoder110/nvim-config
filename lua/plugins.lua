e4fe3075b8015db535c33452657f6f828cbe7bb7
Yogendra Singh Kashyap <yskthecpplover@gmail.com>
2023-02-23 21:27:53 +0530

    lazy = false for vim-fugitive or the plugin wasn't loading.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -127,6 +127,31 @@ require "lazy".setup({
                         end)
                         return "<Ignore>"
                     end, { expr = true })
+                    map("n", "[c", function()
+                        if vim.wo.diff then
+                            return "[c"
+                        end
+                        vim.schedule(function()
+                            gs.prev_hunk()
+                        end)
+                        return "<Ignore>"
+                    end, { expr = true })
+                    -- Actions
+                    map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+                    map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+                    map("n", "<leader>hS", gs.stage_buffer)
+                    map("n", "<leader>hu", gs.undo_stage_hunk)
+                    map("n", "<leader>hR", gs.reset_buffer)
+                    map("n", "<leader>hp", gs.preview_hunk)
+                    map("n", "<leader>hb",
+                        function() gs.blame_line { full = true } end)
+                    map("n", "<leader>tb", gs.toggle_current_line_blame)
+                    map("n", "<leader>hd", gs.diffthis)
+                    map("n", "<leader>hD", function() gs.diffthis "~"  end)
+                    map("n", "<leader>td", gs.toggle_deleted)
+
+                    -- Text object
+                    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
                 end,
             }
         end,
@@ -232,6 +257,7 @@ require "lazy".setup({
     },
     {
         "tpope/vim-fugitive",
+        lazy = false,
         keys = {
             { "<leader>gs", ":Git<cr>" },
             { "<leader>gd", ":Gdiff<cr>" },
