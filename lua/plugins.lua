7c027f8342ed045127fc41bc626e30c66b6770f6
Yogendra Singh Kashyap <yskthecpplover@gmail.com>
2023-02-25 20:39:49 +0530

    Added treesitter-textobjects

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -315,6 +315,65 @@ require "lazy".setup({
         end,
     },
     {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require "nvim-treesitter.configs".setup {
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = "@class.inner",
+                            ["as"] = "@scope",
+                        },
+                        selection_modes = {
+                            ["@parameter.outer"] = "v",
+                            ["@function.outer"] = "V",
+                            ["@class.outer"] = "<c-v>",
+                        },
+                    },
+                    move = {
+                        enable = true,
+                        set_jumps = true,
+                        goto_next_start = {
+                            ["]m"] = "@function.outer",
+                            ["]]"] = "@class.outer",
+                            ["]o"] = "@loop.*",
+                            ["]s"] = "@scope",
+                            ["it"] = "@attribute.inner",
+                            ["iT"] = "@attribute.outer",
+                        },
+                        goto_next_end = {
+                            ["]M"] = "@function.outer",
+                            ["]["] = "@class.outer",
+                        },
+                        goto_previous_start = {
+                            ["[m"] = "@function.outer",
+                            ["[["] = "@class.outer",
+                        },
+                        goto_previous_end = {
+                            ["[M"] = "@function.outer",
+                            ["[]"] = "@class.outer",
+                        }
+                    },
+                    lsp_interop = {
+                        enable = true,
+                        border = "none",
+                        floating_preview_opts = {},
+                        peek_definition_code = {
+                            ["<leader>df"] = "@function.outer",
+                            ["<leader>dF"] = "@class.outer",
+                        }
+                    }
+                }
+            }
+        end
+    },
+    {
         "kylechui/nvim-surround",
         event = "VeryLazy",
         config = function()
